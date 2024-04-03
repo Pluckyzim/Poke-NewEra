ee45689290d05119b020070e2191612ec4583ce6
Pluckyzim <101812234+Pluckyzim@users.noreply.github.com>
2024-04-03 13:46:08 -0300

    Add files via upload

---
+++ dev-cpp/PokeWish_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef POKEWISH_PRIVATE_H
+#define POKEWISH_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.0"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"PokeWish"
+#define FILE_VERSION	"0"
+#define FILE_DESCRIPTION	"PokeWish"
+#define INTERNAL_NAME	"PokeWish"
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"PokeWish.exe"
+#define PRODUCT_NAME	"PokeWish"
+#define PRODUCT_VERSION	"0"
+
+#endif /*POKEWISH_PRIVATE_H*/
